dbee9e6f3bdfc6b41de959f03acf0a249755692d
timattt <timattt@gmail.com>
2025-04-07 18:59:55 +0300

    resolving #155
    limit workers in playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,6 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+    workers: 1,
     testDir: './src/tests/playwright',
     /* Run tests in files in parallel */
     fullyParallel: true,
@@ -19,8 +20,6 @@ export default defineConfig({
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
     retries: process.env.CI ? 2 : 0,
-    /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: 'html',
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
