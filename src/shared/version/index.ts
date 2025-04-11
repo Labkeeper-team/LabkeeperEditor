761d531ef63f2a0ddfaa6a40856850b89e405c3e
timattt <timattt@gmail.com>
2025-04-11 17:12:36 +0300

    resolved #175
    1. changed docker build model
    2. create production environment docker image
    3. used that new image as parent for app docker image
    4. remove maven frontend plugin
    5. swapped to npm instead of yarn
    6. added better image comparison
    7. added new pipeline

--- src/shared/version/index.ts
+++ src/shared/version/index.ts
@@ -1,19 +1,6 @@
-const DEFAULT_API_VERSION: BuildInfo = {
-    major: 2,
-    minor: '',
-};
-const PREFIX = '{';
-
-export const BUILD_INFO = parseVersionEnv(__MODE__ as string);
+export const BUILD_INFO = __BUILD_INFO__ as unknown as BuildInfo;
 
 export interface BuildInfo {
-    major: number;
+    major: string;
     minor: string;
-}
-
-function parseVersionEnv(env: string): BuildInfo {
-    if (env && env.startsWith(PREFIX)) {
-        return JSON.parse(env);
-    }
-    return DEFAULT_API_VERSION;
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,26 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig(({ mode }) => {
+    const DEFAULT = 'unknown';
+    const revision = mode as string;
+    let major = DEFAULT;
+    let minor = DEFAULT;
+    if (revision) {
+        try {
+            const divs: string[] = revision.split('.');
+            major = divs[0];
+            minor = divs.slice(1).join('.');
+        } catch {
+            // ignored
+        }
+    }
     return {
         plugins: [react(), svgr()],
         server: {
             port: 3000,
         },
         define: {
-            __MODE__: JSON.stringify(mode),
+            __BUILD_INFO__: JSON.stringify({ major: major, minor: minor }),
         },
     };
 });
