bffb81883d6dba458d00211088a06843e304d75b
godba11 <nkhudkov@softmedialab.com>
2025-07-17 23:52:18 +0500

    update screen

--- src/view/components/scaleWrapper/index.tsx
+++ src/view/components/scaleWrapper/index.tsx
@@ -1,11 +1,26 @@
 // components/ScaleWrapper.jsx
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { useScaleToMinWidth } from '../../hooks/useScaleToMinWidth';
 
 export default function ScaleWrapper({ minWidth = 1024, children }) {
     const ref = useRef(null);
     useScaleToMinWidth(ref, minWidth);
 
+    useEffect(() => {
+        const updateInnerHeight = () => {
+            document.documentElement.style.setProperty(
+                '--inner-height',
+                `${window.innerHeight}px`
+            );
+        };
+
+        updateInnerHeight();
+
+        window.addEventListener('resize', updateInnerHeight);
+        return () => {
+            window.removeEventListener('resize', updateInnerHeight);
+        };
+    }, []);
     return (
         <div
             ref={ref}
