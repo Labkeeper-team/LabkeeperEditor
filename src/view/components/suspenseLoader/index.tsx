672d3712f6d10b3413d80f0a09ffd607c7994280
godba11 <nkhudkov@softmedialab.com>
2025-10-14 23:07:37 +0500

    Плавная загрузка проекта

---
+++ src/view/components/suspenseLoader/index.tsx
@@ -0,0 +1,7 @@
+import './style.scss';
+
+export const SuspenseLoader = () => {
+    return <div className="suspnese-loader ">
+        <div className="spinner"></div>
+    </div>
+}
--- src/view/pages/project/index.tsx
+++ src/view/pages/project/index.tsx
@@ -35,3 +35,5 @@ export const ProjectPage = () => {
         </div>
     );
 };
+
+export default ProjectPage;
