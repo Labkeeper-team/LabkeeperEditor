eb935b56d3fa17e650680505e75ee9a1fe699ee6
nagelandrey <nageldyuha@gmail.com>
2026-08-03 13:21:57 +0700

    fix: Исправлен нейминг для простой формулы в меню создания сегмента

--- src/viewModel/dictionaries/en.ts
+++ src/viewModel/dictionaries/en.ts
@@ -14,7 +14,7 @@ export const en: Translations = {
 
     warning_dontuselongvarioables: 'Do not use long name variables',
 
-    label_add_asciimath: 'Simple-math',
+    label_add_asciimath: 'Simple-formula',
     label_add_markdown: 'Markdown',
     label_add_markdown_short: 'MD',
     label_add_code: 'Computation',
@@ -36,7 +36,7 @@ export const en: Translations = {
         computation: 'Computation',
         markdown: 'Markdown',
         latex: 'Latex',
-        asciimath: 'Simple-math',
+        asciimath: 'Simple-formula',
     },
     latex_boundary: {
         header: 'LaTeX heading',
@@ -117,7 +117,7 @@ export const en: Translations = {
             non_authorized_paste_image: 'You need authorize to paste images',
         },
         latex: 'latex',
-        asciimath: 'simple-math',
+        asciimath: 'simple-formula',
         no_computation_result: 'No computation result',
         run_to_view: 'Press the run button to see computations',
         hide_assignment: 'Hide the main formula used for calculations',
