b755dd175c80ee3a9ec86eff4ef9938e80b93f13
timattt <timattt@gmail.com>
2026-09-15 16:50:52 +0300

    fix

--- src/tests/jest/web/server.test.ts
+++ src/tests/jest/web/server.test.ts
@@ -31,6 +31,10 @@ jest.mock('axios', () => ({
         post: jest.fn(),
         get: jest.fn(),
         delete: jest.fn(),
+        interceptors: {
+            request: { use: jest.fn() },
+            response: { use: jest.fn() },
+        },
     },
 }));
 
